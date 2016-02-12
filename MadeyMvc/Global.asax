8543401ae6e8930701ea5f27eec1f5a172946f9c
tomaszmadeyski <tomasz.madeyski@gmail.com>
2016-02-12 14:23:38 +0100

    initial commit

---
+++ MadeyMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MadeyMvc.MvcApplication" Language="C#" %>
---
+++ MadeyMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MadeyMvc.Startup))]
+namespace MadeyMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
