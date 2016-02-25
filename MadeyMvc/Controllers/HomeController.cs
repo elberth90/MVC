9eb7dbad3384c63b5af4a2b28abdc2bfa50d5606
tomaszmadeyski <tomasz.madeyski@gmail.com>
2016-02-25 12:34:02 +0100

    5

--- MadeyMvc/Controllers/HomeController.cs
+++ MadeyMvc/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MadeyMvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page 3.";
+            ViewBag.Message = "Your application description page 5.";
 
             return View();
         }
